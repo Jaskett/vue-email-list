b3d0ddc650e1427fdc2e2e62b72a62bc8beae33e
Brandon Vasquez <thatsjaskett@gmail.com>
2023-01-31 16:20:39 +0100

    Inserimento eMail

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,18 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            currEMail: '',
+            emailArray: []
         }
+    },
+    created() {
+        for(let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response => {
+                    this.currEMail = response.data.response;
+                    this.emailArray.push(this.currEMail);
+                })
+        }
+        console.log(this.emailArray);
     }
 }).mount('#app')
